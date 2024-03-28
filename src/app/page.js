d71c975f0dda70ac2c441dcfccc55ca00717f08e
John Keenan <john.keenan@dcmail.ca>
2024-03-28 15:32:51 -0400

    fix map conditonal render

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import Image from "next/image";
 export default function Home() {
 	return (
 		<main className={styles.appBody}>
-			{typeof(window) ? <Map /> : undefined}
+			{typeof(window) !== "undefined" ? <Map /> : undefined}
 			<Image
 				className='mainLogo'
 				src='/assets/header_logo.svg'
