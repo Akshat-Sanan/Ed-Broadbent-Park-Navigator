bb93e76c973c45a649a19c132b917acbf3df8f93
John Keenan <87619864+Airwolf435@users.noreply.github.com>
2024-03-28 22:32:59 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 // https://www.youtube.com/watch?v=mJuz45RXeXY helped get the github pages deployment fixed
 const nextConfig = {
+  output: "export",
   basePath: process.env.NODE_ENV === "production" ? "/teaching-city-broadbent-park" : "",
   distDir: './out'
 };
