3b68db3780daf84a4f79c2b1a637fe9f2e867d12
John Keenan <jgknomad@gmail.com>
2024-03-30 22:45:04 -0400

    Final info gallery header link fixes

--- src/app/info/infoGallery/page.js
+++ src/app/info/infoGallery/page.js
@@ -6,6 +6,6 @@ export default function InfoGalleryPage() {
     const images = poiData[4].pages.filter(page => page.type === 'gallery')[0].images.map(image => image.bannerImage);
 
     return <>
-        <Gallery header={"ED BROADBENT WATERFRONT PARK"} images={images} backPath="/info" numberOfImages={images.length} />
+        <Gallery header={"ED BROADBENT WATERFRONT PARK"} images={images} numberOfImages={images.length} />
     </>
 }
